b0fa4e217acb22d9bc59965ae72a822244ca8b95
masaki4680 <masaki46803980@gmail.com>
2022-10-16 21:29:24 +0900

    init2

--- public/js/baz.js
+++
@@ -1,6 +0,0 @@
-export function addBaz(p) {
-    const baz = document.createElement("p");
-    baz.textContent = "It is Baz. change test";
-    p === null || p === void 0 ? void 0 : p.appendChild(baz);
-}
-//# sourceMappingURL=baz.js.map
--- public/js/index.js
+++
@@ -1,7 +0,0 @@
-import { addBaz } from "./baz.js";
-window.onload = function () {
-    const app = document.getElementById("foo");
-    app.innerHTML = "Hello Typescript World! foo";
-    addBaz(app);
-};
-//# sourceMappingURL=index.js.map
